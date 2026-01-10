a6ce967cc92ddcb62bed6006b2cb90f540fbd77d
Prer010 <prerp.6299@gmail.com>
2026-01-11 00:05:21 +0530

    Google OAuth Error fix

--- config/passport.js
+++ config/passport.js
@@ -20,7 +20,8 @@ export function configurePassport(db) {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: process.env.GOOGLE_CALLBACK_URL
+        callbackURL: process.env.GOOGLE_CALLBACK_URL,
+        scope: ['profile', 'email']
     },
         async (accessToken, refreshToken, profile, done) => {
             try {
